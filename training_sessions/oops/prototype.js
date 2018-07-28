81494e5af33c99cd1450078cbefe235aab332aca
chaitanyaubijawe <chaitanyaubijawe@gmail.com>
2018-07-28 17:24:41 +0530

    Update prototype.js
    changes to see prototype information.....

--- training_sessions/oops/prototype.js
+++ training_sessions/oops/prototype.js
@@ -31,6 +31,9 @@ console.log(n.toLowerCase(), String.prototype);
 
 // ECMA6- 2015
 
+
+
+
 class Person1{
 
 
@@ -49,3 +52,81 @@ class Person1{
 }
 var p = new Person1("person")
 console.log(p.getName());
+
+
+
+
+
+//////////////////////////////////////////////
+
+
+
+"use strict";
+// var a; variable hoisting.....
+
+console.log(a)
+var a = 3;
+m1();
+
+/*
+
+function hoisting.....
+var m1 = function(){
+	
+	console.log("Inside m1");
+}
+*/
+
+
+function m1(){
+	
+	console.log("Inside m1");
+}
+
+function Person(name, lName)
+{
+
+	this.name = name;
+	this.lName = lName;
+
+}
+
+
+var sneh = new Person("Sneh", "Roy");
+var shrutika = new Person("Shrutika", "Pukale");
+
+console.log("Peron.name :: --  ", sneh.name, shrutika.name);
+
+console.log(this);
+
+console.log("This accessing sneh...", this.sneh)
+console.log("########################################")
+
+
+function M(name){
+
+	this.name = name;
+	console.log("hi there ***** ...", this);
+
+	function inside_m(){
+		console.log("inside class  method...");
+	
+	}
+
+	inside_m();
+}
+
+var name = "abcd";
+var m = new M(name);
+
+M.prototype.getName = function(){
+
+	console.log("This is a function");
+	return "calling from ref variable";
+}
+
+
+//console.log("Dekhob bhai...", )
+
+m.getName();
+
