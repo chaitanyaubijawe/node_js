a5573d6a61a07f17681832436fab17b3f828e630
chaitanyaubijawe <chaitanyaubijawe@gmail.com>
2018-07-29 17:17:48 +0530

    Create varScopeChain.js
    scope chain related changes

---
+++ training_sessions/varScopeChain.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+
+
+function m4(){
+	
+	console.log("This is m4");
+	
+	var ab = "this is jst a variable."
+	function m5(){
+		
+		console.log("this is m5", ab);
+		}
+
+	m5();
+
+	
+	
+}
+
+
+m4();
+
