f7218e83abcc37ae407ffb50d6c65a7c5f4209c7
Chaitanya Bijawe <chaitanya.bijawe@pb.com>
2018-08-01 20:37:29 +0530

    session on wmap and db queries

---
+++ training_sessions/sonali/db.js
@@ -0,0 +1,16 @@
+var selectQuery = "SELECT * FROM student WHERE id = 1";
+var selectQuery = "SELECT * FROM student WHERE id = 2";
+var selectQuery = "SELECT * FROM student WHERE id = 3";
+var selectQuery = "SELECT * FROM student WHERE id = 4";
+var selectQuery = "SELECT * FROM student WHERE id = 10";
+
+
+// generateSelectQuery(numOfQuery); return array of [selectQuery];
+
+var insertQuery = "INSERT INTO student(id, name, lName, age, phNumber) VALUES (1,'Name-1', 'LName-1', '1', '1')"
+var insertQuery = "INSERT INTO student(id, name, lName, age, phNumber) VALUES (2,'Name-2', 'LName-2', '2', '2')"
+var insertQuery = "INSERT INTO student(id, name, lName, age, phNumber) VALUES (10,'Name-10', 'LName-10', '10', '10')"
+
+// var studentInfo = {"id":"1", "name":"Name-1", "lName":"lName-1", "age":"1", "phNumber":"1"};
+// generateInsertQuery(studentInfo);
+// generateInsertQuery({"id":"2", "name":"Name-2", "lName":"lName-2", "age":"2", "phNumber":"2"}) return insertQuery;
---
+++ training_sessions/sonali/world_map.js
@@ -0,0 +1,110 @@
+var w_map = {
+  "IND": {
+    "MAH": ["PUNE", "Mumbai"],
+    "MP": ["Jabalpur", "Indore"],
+    "UP": ["Agra", "Jhansi"]
+  }
+}
+
+var wMap = {};
+var sMap = {};
+var mahCities = [];
+// mahCities = ["Pune"]
+
+var mpCities = [];
+var upCities = [];
+
+
+
+mahCities.push("Pune");
+mahCities.push("Mumbai");
+
+mpCities.push("Jabalpur");
+mpCities.push("Indore");
+
+upCities.push("Jhansi");
+upCities.push("Agra");
+
+
+sMap["MAH"] = mahCities;
+sMap["MP"] = mpCities;
+sMap["UP"] = upCities;
+
+wMap["IND"] = sMap;
+
+
+//console.log(wMap);
+
+
+var populateCities = function(stateName) {
+  // db connection. it will execute a qiuery in dnb // select * from cities where state = "MAH";
+  var cities = [];
+
+  for (var num = 0; num <= 9; num++) {
+    var city = stateName + "-city-" + num;
+    cities.push(city);
+  }
+  return cities;
+}
+
+
+var populateStateMap = function() {
+  var states = ["MAH", "UP", "MP"];
+  var sMap = {};
+
+  for (var state of states) {
+
+    sMap[state] = populateCities(state);
+  }
+
+  return sMap;
+}
+
+var populateWMap = function() {
+  var countries = ["IND"];
+  var wMap = {}
+
+  for (var country of countries) {
+
+
+    wMap[country] = populateStateMap();
+    //wMap["IND"] = sMap;
+  }
+
+  return wMap;
+
+}
+
+var displayWMap = function() {
+
+  var wMap = populateWMap();
+
+  for (var country in wMap) {
+
+    console.log(country);
+    var sMap = wMap[country];
+
+    for (var state in sMap) {
+      console.log('\t', state);
+      var cities = sMap[state];
+
+      for (var city of cities) {
+        console.log('\t\t', city);
+      }
+    }
+  }
+}
+
+displayWMap();
+
+// TODO: populateStateMap(countryName)
+// TODO: populateCities(stateName, countryName)// "IND-MAH-Citi-1", "IND-MAH-Citi-2"...."IND-MP-Citi-1"
+
+// Ind:
+// MAH:
+//     PUNE
+//     Mumbai
+// MP:
+//     Indore
+// UP:
+//
