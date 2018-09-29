d2929f504302e03308b20e1665eae63cc51d8dd2
Chaitanya Bijawe <chaitanya.bijawe@pb.com>
2018-09-29 16:48:13 +0530

    changes to data binding...

--- flipkart-angular/src/app/add-product/add-product.component.ts
+++ flipkart-angular/src/app/add-product/add-product.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../model/product';
+
+
 
 @Component({
   selector: 'app-add-product',
+  // selector: '[app-add-product]',
+  // selector: '.app-add-product',
   templateUrl: './add-product.component.html',
+  //template: `<h3> This is add product page...</h3>`,
   styleUrls: ['./add-product.component.css']
+  // styles:[`h3{
+  // color: blueviolet;
+  // }`]
 })
 export class AddProductComponent implements OnInit {
 
-  constructor() { }
+  stringInterpolation:string = "String property binding example";
+  shouldEnable:boolean = true; // this is property binding....
+  biDirectional:string = "This is biderectional binding....";
+  product = {};
+  productTS:Product = new Product();
+  constructor() {
 
-  ngOnInit() {
+    console.log("Inside constructor :: ")
   }
 
+  ngOnInit() {
+
+    console.log("Inside ngOnInit :: ")
+  }
+
+
+  onInputChange(event:any){
+
+    //console.log(event.target.value);
+
+    this.biDirectional = event.target.value;
+  }
+
+  addProduct(){
+
+  console.log(JSON.stringify(this.productTS));
+
+  }
 }
