ac15f7edcd8c1b67e06a2774913993e5481e138c
rajputaarti81 <rajputaarti@gmail.com>
2018-11-18 07:56:22 +0530

    Updated ex1.js

--- batch_2/session_1/ex1.js
+++ batch_2/session_1/ex1.js
@@ -2,11 +2,20 @@
 // string "1,2,3,5";
 // string "1:2:3:5";
 // string "1|2|3|5";
+arr = [1,2,3,5];
 
-let arr = [1, 2, 3, 4, 5];
-console.log("string " + `"` + arr.join(",") + `"`);
-console.log("string " + `"` + arr.join(":") + `"`);
-console.log("string " + `"` + arr.join("|") + `"`);
+let opp = arr.join(",");
+//console.log("Using join method:: ", opp, typeof(opp));
+
+let output = ""
+let sep = ","
+
+for (let index in arr){
+console.log("Index value is ::", index, arr[index]);
+output = output + sep + arr[index];
+}
+output = '"' + output.substring(1) + '"'
+console.log("Output is ::", output);
 
 // Exercise 2
 
@@ -14,7 +23,12 @@ console.log("string " + `"` + arr.join("|") + `"`);
 //  string:: "key_1 = value_1, key_2 = value_2, key_3 = value_3;"
 
 obj = {"Aarti":"12", "Rajput":"15"};
-console.log(JSON.stringify(obj));
-for(let key in obj){
-  console.log("string :: " + `"` + key +'='+ obj[key] +`"`);
+
+let output_1 = ""
+
+for(let key in obj) {
+  console.log("Key of the Object:: ", key , obj[key]); /// ask about value
+  output_1 = output_1 + '"' + key + "=" + obj[key] + '"' + ",";
 }
+
+console.log("Output is::", output_1.slice(0, -1));
