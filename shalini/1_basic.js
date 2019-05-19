8c14a8a4b73070101abfb0ba40ea78cabc577c02
Chaitanya Bijawe <chaitanya.bijawe@pb.com>
2019-05-19 12:20:52 +0530

    changes for data types

--- shalini/1_basic.js
+++ shalini/1_basic.js
@@ -1,1 +1,35 @@
 console.log("Hi there!!!");
+// reference variable....
+var name = "Shalini"; // data type : String....
+var age = 25; // data type is number...
+var lName = " Kumari";
+
+name  = name + lName;
+// = is assignment operator....
+// + concatination operator...
+
+var productName = "Iphone"
+var seoBest = "BEST"
+
+
+console.log(name);
+console.log(age);
+
+// arithmatic operators
+var result = age + 2;
+
+console.log(result);
+
+var seoKeywordsArr = [];
+seoKeywordsArr = [1,2,4,5]
+seoKeywordsArr = ["BEST", "IPHONE", "BEST PRICE", "OFFER"]
+
+
+var eComProducts = ["Iphone 7", "OnPlus7", "Samsung s10"]
+
+var iphone = {"prductName":"Iphone 7", "price":50000, "brand":"apple"};
+var onePlus = {"prductName":"OnePlus 7", "price":50000, "brand":"onePlus"};
+
+var productsArr = [iphone, onePlus];
+
+console.log(productsArr);
