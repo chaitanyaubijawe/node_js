141c4c760822d6379b48965846c9186f59728ce4
Chaitanya Bijawe <chaitanya.bijawe@pb.com>
2019-06-17 14:43:27 +0530

    changes for routing

--- batch_3/session_6_angular/e-cart/src/app/app-routing.module.ts
+++ batch_3/session_6_angular/e-cart/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {ProductDetailsComponent} from "./product-details/product-details.component";
+import {AddProductDetailsComponent} from "./add-product-details/add-product-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:DashboardComponent},
+  {path:"viewProduct", component:ProductDetailsComponent},
+  {path:"addProduct", component:AddProductDetailsComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
